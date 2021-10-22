f60196accfe89176d053abdc518d6e91fc046944
OfekShochat <o@shoch.at>
2021-10-22 17:07:10 +0300

    graceful shutdown for threads

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ use std::io::Read;
 use std::process::exit;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
-use std::time::Duration;
 use std::{fs::File, str::FromStr};
 use tch::{
   nn::{self, Module, OptimizerConfig},
@@ -25,7 +24,6 @@ use tch::{
 };
 use toml::from_str;
 
-const DEADLINE: u64 = 100;
 const SCALE: f32 = 1024.0;
 
 fn create_net(vs: &nn::Path) -> impl Module {
@@ -49,8 +47,7 @@ fn main() {
 
   let mut opt = nn::AdamW::default().build(&vs, config.training.lr).unwrap();
 
-  let mut data = Data::new(config.training.batch_size);
-  data.start(config.workers);
+  let mut data = Data::new(config.training.batch_size, config.workers);
   let mut running_loss = Tensor::of_slice(&[0.0])
     .to_device(Device::cuda_if_available())
     .detach();
@@ -203,33 +200,33 @@ fn data_worker(sender: Sender<Datapoint>) {
     }
     let dp = Datapoint::from_string(l.unwrap().unwrap());
     if dp.is_some() {
-      sender.send(dp.unwrap()).unwrap();
+      match sender.send(dp.unwrap()) {
+        Err(_) => return,
+        _ => {}, 
+      }
     }
   }
 }
 
 struct Data {
-  send: Sender<Datapoint>,
   recv: Receiver<Datapoint>,
   batch_size: usize,
 }
 
 impl Data {
-  pub fn new(batch_size: usize) -> Data {
+  pub fn new(batch_size: usize, workers: usize) -> Data {
     let (send, recv) = channel();
-    Data {
-      send,
-      recv,
-      batch_size,
-    }
-  }
-
-  pub fn start(&mut self, workers: usize) {
+    
     for _ in 0..workers {
-      let sender_cp = self.send.clone();
+      let sender_cp = send.clone();
       thread::spawn(move || {
         data_worker(sender_cp);
       });
+    }
+
+    Data {
+      recv,
+      batch_size,
     }
   }
 }
