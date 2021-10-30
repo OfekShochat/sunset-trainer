12db462254d9a7d2eba7561e275fa62a8c46d317
OfekShochat <o@shoch.at>
2021-10-30 14:31:42 +0300

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,20 @@ use ctrlc::set_handler;
 use easy_reader::EasyReader;
 use fastapprox::fast::sigmoid;
 use serde_derive::Deserialize;
-use tch::Kind;
-use tch::nn::ModuleT;
-use torch_sys::at_tensor_of_data;
 use std::ffi::c_void;
 use std::io::Read;
 use std::process::exit;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
 use std::{fs::File, str::FromStr};
+use tch::nn::ModuleT;
+use tch::Kind;
 use tch::{
   nn::{self, Module, OptimizerConfig},
   Device, Tensor,
 };
 use toml::from_str;
+use torch_sys::at_tensor_of_data;
 
 const SCALE: f32 = 1024.0;
 
@@ -34,9 +34,10 @@ fn get_conv_config() -> nn::ConvConfigND<[i64; 2]> {
     stride: [1, 1],
     padding: [1, 1],
     dilation: [1, 1],
-    bias: true,groups: 1,
+    bias: true,
+    groups: 1,
     ws_init: nn::Init::KaimingUniform,
-    bs_init: nn::Init::Const(0.)
+    bs_init: nn::Init::Const(0.),
   }
 }
 
@@ -51,10 +52,30 @@ struct ResidualBlock {
 impl ResidualBlock {
   pub fn new(vs: &nn::Path, index: usize, inputs: i64, filters: i64) -> ResidualBlock {
     ResidualBlock {
-      conv0: nn::conv(vs / format!("{}/conv0", index), inputs, filters, [3, 3], get_conv_config()),
-      bn0: nn::batch_norm2d(vs / format!("{}/bn0", index), filters, nn::BatchNormConfig::default()),
-      conv1: nn::conv(vs / format!("{}/conv1", index), inputs, filters, [3, 3], get_conv_config()),
-      bn1: nn::batch_norm2d(vs / format!("{}/bn1", index), filters, nn::BatchNormConfig::default()),
+      conv0: nn::conv(
+        vs / format!("{}/conv0", index),
+        inputs,
+        filters,
+        [3, 3],
+        get_conv_config(),
+      ),
+      bn0: nn::batch_norm2d(
+        vs / format!("{}/bn0", index),
+        filters,
+        nn::BatchNormConfig::default(),
+      ),
+      conv1: nn::conv(
+        vs / format!("{}/conv1", index),
+        inputs,
+        filters,
+        [3, 3],
+        get_conv_config(),
+      ),
+      bn1: nn::batch_norm2d(
+        vs / format!("{}/bn1", index),
+        filters,
+        nn::BatchNormConfig::default(),
+      ),
     }
   }
 }
@@ -79,8 +100,19 @@ struct ValueHead {
 impl ValueHead {
   pub fn new(vs: &nn::Path, inputs: i64) -> ValueHead {
     ValueHead {
-      conv0: nn::conv(vs / "valuehead/conv0", inputs, 32, [1, 1], get_conv_config()),
-      dense0: nn::linear(vs / "valuehead/dense0", 3200, 128, nn::LinearConfig::default()),
+      conv0: nn::conv(
+        vs / "valuehead/conv0",
+        inputs,
+        32,
+        [1, 1],
+        get_conv_config(),
+      ),
+      dense0: nn::linear(
+        vs / "valuehead/dense0",
+        3200,
+        128,
+        nn::LinearConfig::default(),
+      ),
       dense1: nn::linear(vs / "valuehead/dense1", 128, 1, nn::LinearConfig::default()),
     }
   }
@@ -104,8 +136,19 @@ struct PolicyHead {
 impl PolicyHead {
   pub fn new(vs: &nn::Path, inputs: i64) -> PolicyHead {
     PolicyHead {
-      conv0: nn::conv(vs / "policyhead/conv0", inputs, 32, [1, 1], get_conv_config()),
-      dense: nn::linear(vs / "policyhead/dense", 3200, 1859, nn::LinearConfig::default()),
+      conv0: nn::conv(
+        vs / "policyhead/conv0",
+        inputs,
+        32,
+        [1, 1],
+        get_conv_config(),
+      ),
+      dense: nn::linear(
+        vs / "policyhead/dense",
+        3200,
+        1859,
+        nn::LinearConfig::default(),
+      ),
     }
   }
 }
@@ -133,9 +176,21 @@ impl NN {
       blocks.push(ResidualBlock::new(&vs.root(), i, 128, 128))
     }
     NN {
-      iconv: nn::conv(&vs.root() / "iconv", 12, filter_count, [3, 3], nn::ConvConfigND::<[i64; 2]> { stride: [1, 1], padding: [1, 1], dilation: [1, 1], bias: true, groups: 1,
-        ws_init: nn::Init::KaimingUniform,
-        bs_init: nn::Init::Const(0.), }),
+      iconv: nn::conv(
+        &vs.root() / "iconv",
+        12,
+        filter_count,
+        [3, 3],
+        nn::ConvConfigND::<[i64; 2]> {
+          stride: [1, 1],
+          padding: [1, 1],
+          dilation: [1, 1],
+          bias: true,
+          groups: 1,
+          ws_init: nn::Init::KaimingUniform,
+          bs_init: nn::Init::Const(0.),
+        },
+      ),
       blocks,
       value_head: ValueHead::new(&vs.root(), filter_count),
       policy_head: PolicyHead::new(&vs.root(), filter_count),
@@ -161,7 +216,9 @@ fn main() {
 
   let vs = nn::VarStore::new(Device::cuda_if_available());
   let net = NN::new(10, 128, &vs);
-  let mut opt = nn::sgd(0.9, 0.0, 0.001 , true).build(&vs, config.training.lr).unwrap();
+  let mut opt = nn::sgd(0.9, 0.0, 0.001, true)
+    .build(&vs, config.training.lr)
+    .unwrap();
 
   let mut data = Data::new(config.training.batch_size, config.workers);
   let mut running_loss = Tensor::of_slice(&[0.0])
@@ -171,7 +228,8 @@ fn main() {
   let output_path = config.output_path.clone();
   set_handler(move || {
     exit(0);
-  }).expect("Error setting Ctrl-C handler.");
+  })
+  .expect("Error setting Ctrl-C handler.");
   for (step, (x, y)) in (&mut data).enumerate() {
     let (value, policy) = net.forward(&x);
     let value_loss = value.mse_loss(&y, tch::Reduction::Mean);
@@ -232,8 +290,12 @@ impl Datapoint {
       let piece = board.piece_on(s);
 
       match color {
-        Some(chess::Color::White) => inputs[piece.unwrap().to_index()][s.to_index() / 8][s.to_index() % 8] = 1.0,
-        Some(chess::Color::Black) => inputs[piece.unwrap().to_index() + 6][s.to_index() / 8][s.to_index() % 8] = 1.0,
+        Some(chess::Color::White) => {
+          inputs[piece.unwrap().to_index()][s.to_index() / 8][s.to_index() % 8] = 1.0
+        }
+        Some(chess::Color::Black) => {
+          inputs[piece.unwrap().to_index() + 6][s.to_index() / 8][s.to_index() % 8] = 1.0
+        }
         None => continue,
       }
     }
@@ -263,7 +325,7 @@ fn data_worker(sender: Sender<Datapoint>) {
     if dp.is_some() {
       match sender.send(dp.unwrap()) {
         Err(_) => return,
-        _ => {}, 
+        _ => {}
       }
     }
   }
@@ -277,7 +339,7 @@ struct Data {
 impl Data {
   pub fn new(batch_size: usize, workers: usize) -> Data {
     let (send, recv) = channel();
-    
+
     for _ in 0..workers {
       let sender_cp = send.clone();
       thread::spawn(move || {
@@ -285,10 +347,7 @@ impl Data {
       });
     }
 
-    Data {
-      recv,
-      batch_size,
-    }
+    Data { recv, batch_size }
   }
 }
 
@@ -322,28 +381,28 @@ impl Iterator for Data {
 pub fn tensor<T>(data: &[T], dims: &[i64], kind: tch::Kind) -> Tensor {
   let t = unsafe {
     Tensor::from_ptr(at_tensor_of_data(
-        data.as_ptr() as *const c_void,
-        dims.as_ptr(),
-        dims.len(),
-        kind.elt_size_in_bytes(),
+      data.as_ptr() as *const c_void,
+      dims.as_ptr(),
+      dims.len(),
+      kind.elt_size_in_bytes(),
       match kind {
-          Kind::Uint8 => 0,
-          Kind::Int8 => 1,
-          Kind::Int16 => 2,
-          Kind::Int => 3,
-          Kind::Int64 => 4,
-          Kind::Half => 5,
-          Kind::Float => 6,
-          Kind::Double => 7,
-          Kind::ComplexHalf => 8,
-          Kind::ComplexFloat => 9,
-          Kind::ComplexDouble => 10,
-          Kind::Bool => 11,
-          Kind::QInt8 => 12,
-          Kind::QUInt8 => 13,
-          Kind::QInt32 => 14,
-          Kind::BFloat16 => 15,
-        },
+        Kind::Uint8 => 0,
+        Kind::Int8 => 1,
+        Kind::Int16 => 2,
+        Kind::Int => 3,
+        Kind::Int64 => 4,
+        Kind::Half => 5,
+        Kind::Float => 6,
+        Kind::Double => 7,
+        Kind::ComplexHalf => 8,
+        Kind::ComplexFloat => 9,
+        Kind::ComplexDouble => 10,
+        Kind::Bool => 11,
+        Kind::QInt8 => 12,
+        Kind::QUInt8 => 13,
+        Kind::QInt32 => 14,
+        Kind::BFloat16 => 15,
+      },
     ))
   };
   t
